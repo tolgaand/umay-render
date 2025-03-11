568c9a874d98e4ec25172a1e6ceff43678cfaf75
Tolga Çağlayan <tolgaand@yandex.com>
2025-03-11 03:36:13 +0300

    fix(umay-web): Improve Vite configuration for Node.js polyfills

--- packages/umay-web/vite.config.ts
+++ packages/umay-web/vite.config.ts
@@ -1,18 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import dotenv from "dotenv";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-dotenv.config();
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), nodePolyfills()],
+
   optimizeDeps: {
     include: ["umay-render"],
   },
-  define: {
-    "process.env": JSON.stringify(process.env),
+
+  build: {
+    commonjsOptions: {
+      include: [/umay-render/, /node_modules/],
+    },
+  },
+
+  resolve: {
+    mainFields: ["module", "main", "browser"],
   },
 });
